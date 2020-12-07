2b279039c9b2e8b2db38b09cdf6838e0bfe816b1
Ricky Viejo <ec2-user@ip-172-31-29-137.us-east-2.compute.internal>
2020-12-07 03:42:26 +0000

    RB101 Lesson 2 - Calculator

---
+++ rb101/lesson_2/calculator.rb
@@ -0,0 +1,28 @@
+# Build a command line calculator program that does the following
+# => asks for two numbers
+# => asks for the type of operation to perform
+# => displays the result
+
+puts "Please provide a number"
+num_a = gets.chomp.to_i
+puts "Please provide another number"
+num_b = gets.chomp.to_i
+
+loop do
+  puts "What operation would you like to perform: add, subtract, multiply, or divide?"
+  operation = gets.chomp.downcase
+  
+  case operation
+    when "divide" then puts num_a / num_b
+    when "multiply" then puts num_a * num_b
+    when "add" then puts num_a + num_b
+    when "subtract" then puts num_a - num_b
+    else 
+      puts "Please provide a valid operation: add, subtract, multiply, or divide"
+      next
+  end
+  
+  break
+end
+
+
---
+++ rb101/lesson_2/truthiness.rb
@@ -0,0 +1,11 @@
+p true.class # => TrueClass
+p true.nil?   # => false
+p true.to_s  # => 'true'
+
+p false && 3/0
+
+
+p true || 2/0
+
+
+
