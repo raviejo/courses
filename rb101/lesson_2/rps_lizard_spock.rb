0edc6a632563ffa708ce332dd5e5a900ee634f64
Ricky Viejo <raviejo@gmail.com>
2021-01-26 09:12:44 -0500

    Finalized rps_lizard_spock game with saved scores and abbreviated player inputs

--- rb101/lesson_2/rps_lizard_spock.rb
+++ rb101/lesson_2/rps_lizard_spock.rb
@@ -40,13 +40,17 @@ scores = {
   computer: 0,
 }
 
-choice = ''  
+player_input = nil
+player_choice = nil  
 loop do
   loop do
     prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = Kernel.gets().chomp()
+    player_input = Kernel.gets().chomp()
+    player_choice = VALID_CHOICES.select {|choice| choice.start_with?(player_input)}.first
 
-    if VALID_CHOICES.include?(choice)
+    if player_input == "s"
+      prompt("Choose 'sc' for scissors or 'sp' for spock")
+    elsif VALID_CHOICES.include?(player_choice)
       break
     else
       prompt("That's not a valid choice")
@@ -55,16 +59,20 @@ loop do
 
   computer_choice = VALID_CHOICES.sample
 
-  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+  Kernel.puts("You chose: #{player_choice}; Computer chose: #{computer_choice}")
 
-  display_results(choice, computer_choice, rules_table)
-  update_score(choice, computer_choice, rules_table, scores)
+  display_results(player_choice, computer_choice, rules_table)
+  update_score(player_choice, computer_choice, rules_table, scores)
   display_score(scores)
   winner = scores.select { |k, v| k if v == 5 }.keys.first.to_s
 
   if scores.has_value?(5)
+    reset_score(scores)
     prompt("#{winner} is the winner")
-    break
+    prompt("Do you want to keep playing?")
+    answer = Kernel.gets().chomp()
+    break if answer.downcase.start_with?('n')
   end
+
 end
 prompt('Thank you for playing')
