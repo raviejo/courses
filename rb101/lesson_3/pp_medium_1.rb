63133138cd3ae52132757e7b7e35c729dd59929a
Ricky Viejo <raviejo@gmail.com>
2021-01-26 17:30:01 -0500

    Renaming practice file to correct section number

---
+++ rb101/lesson_3/pp_medium_1.rb
@@ -0,0 +1,59 @@
+require 'pry'
+
+# Medium Problems 1
+# Question 3
+
+def factors(number)
+  divisor = number
+  factors = []
+  while divisor > 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors
+end
+
+
+# Question 4
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+def rolling_buffer2(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+# Question 5
+limit = 15
+
+def fib(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1, limit)
+puts "result is #{result}"
+
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
