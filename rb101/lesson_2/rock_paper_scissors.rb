74b092eaee32ca5077446fece423041e73e1ccd7
Ricky Viejo <ricardo.viejo@roberthalf.com>
2021-01-25 20:32:10 -0500

    Walk-through: Rock Paper Scissors

--- rb101/lesson_2/rock_paper_scissors.rb
+++ rb101/lesson_2/rock_paper_scissors.rb
@@ -4,31 +4,41 @@ def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+winner = {
+  paper: 'rock',
+  scissors: 'paper',
+  rock: 'scissors'
+}
 
-choice = ''
-loop do
-  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-  choice = Kernel.gets().chomp()
-
-  if VALID_CHOICES.include?(choice)
-    break
-  else
-    prompt("That's not a valid choice")
+def display_results(player, computer, winner)
+  if player == computer then prompt("It's a tie!")
+  elsif winner[player.intern] == computer then prompt("Player wins!")
+  else prompt("Computer wins!")
   end
 end
 
-computer_choice = VALID_CHOICES.sample
+choice = ''
+loop do
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = Kernel.gets().chomp()
 
-Kernel.puts("You cohse: #{choice}; Computer chose: #{computer_choice}")
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt("That's not a valid choice")
+    end
+  end
 
-if (choice == 'rock' && computer_choice == 'scissors') ||
-    (choice == 'paper' && computer_choice == 'rock') ||
-    (choice == 'scissors' && computer_choice == 'paper')
-  prompt('You won!')
-elsif (choice == 'rock' && computer_choice == 'paper') ||
-  (choice == 'paper' && computer_choice == 'scissors') ||
-  (choice == 'scissors' && computer_choice == 'rock')
-  prompt("Computer won!")
-else
-  prompt("It's a tie!")
+  computer_choice = VALID_CHOICES.sample
+
+  Kernel.puts("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_results(choice, computer_choice, winner)
+
+  prompt('Do you want to play again? (y/n)')
+  answer = Kernel.gets().chomp()
+  break unless answer.downcase().start_with?('y')
 end
+
+prompt('Thank you for playing')
