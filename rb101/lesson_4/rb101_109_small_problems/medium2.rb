24d816a6741d90a0062ab40dbd5d9b3fececd55c
Ricardo Viejo <raviejo@gmail.com>
2021-04-01 19:20:55 +0000

    Finished Medium 2 Problem 2

--- rb101/lesson_4/rb101_109_small_problems/medium2.rb
+++ rb101/lesson_4/rb101_109_small_problems/medium2.rb
@@ -1,8 +1,9 @@
 ## 1. Longest Sentence
+# Problem:
 # Write a program that reads the content of a text file and then prints the longest sentence in the file based on number of words. Sentences may end with periods (.), exclamation points (!),
 # or question marks (?). Any sequence of characters that are not spaces or sentence-ending characters should be treated as a word. You should also print the number of words in the longest
 # sentence.
-# Example text:
+# Examples:
 # Four score and seven years ago our fathers brought forth
 # on this continent a new nation, conceived in liberty, and
 # dedicated to the proposition that all men are created
@@ -42,6 +43,7 @@
 
 # The longest sentence in this book is 157 words long.
 
+# Downloaded the book and saved it as "text.txt"
 text = File.read("text.txt")
 
 def longest_sentence(text)
@@ -53,3 +55,46 @@ def longest_sentence(text)
 end
 
 longest_sentence(text)
+
+# 2. Now I Know My ABCs
+# A collection of spelling blocks has two letters per block, as shown in this list:
+# B:O   X:K   D:Q   C:P   N:A
+# G:T   R:E   F:S   J:W   H:U
+# V:I   L:Y   Z:M
+# This limits the words you can spell with the blocks to just those words that do not use both letters from any given block. Each block can only be used once.
+# Write a method that returns true if the word passed in as an argument can be spelled from this set of blocks, false otherwise.
+# Examples:
+# block_word?('BATCH') == true
+# block_word?('BUTCH') == false
+# block_word?('jest') == true
+
+
+SPELLING_BLOCKS = %w(BO XK DQ CP NA GT RE FS JW HU VI LY ZM)
+
+def block_word?(word)
+  counter = 0
+  SPELLING_BLOCKS.each do |block| 
+    word.each_char do |letter|
+      counter += 1 if block.include?(letter.upcase)
+    end
+    if counter >= 2
+      return false
+    else
+      counter = 0
+    end
+  end
+  return true
+end
+
+def block_word(word)
+  blocks = SPELLING_BLOCKS
+  !blocks.map do |block|
+    word.upcase.count(block) >= 2
+  end.include?(true)
+end
+
+def block_word?(word)
+  blocks = SPELLING_BLOCKS
+  blocks.none? {|block| word.upcase.count(block) >= 2}
+end
+
