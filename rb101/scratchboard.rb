375a888711f18d966f5bce48a47aa9cd8a7ca2de
Ricky Viejo <raviejo@gmail.com>
2021-04-14 13:53:53 -0400

    More practice problems

--- rb101/scratchboard.rb
+++ rb101/scratchboard.rb
@@ -158,3 +158,102 @@ p (['a b', 'c z', 'f m'].sort_by do |word|
   word[1]
 end)
 
+=begin
+Given an integer n, find the maximal number you can obtain by deleting exactly one digit of the given number.
+
+Example
+For n = 152, the output should be 52;
+
+For n = 1001, the output should be 101.
+
+Input/Output
+[input] integer n
+
+Constraints: 10 ≤ n ≤ 1000000.
+
+[output] an integer
+=end
+p delete_digit(152) == 52
+p delete_digit(1001) == 101
+p delete_digit(10) == 1
+
+# Algorithm
+# - Split up each number into a collection of its digits
+# - Find all possible combinations of the size of the collection minus one
+#   - For example, if the collection has 3 items, find all pairs. If it has 4 items, find all triplets.
+#   - Iterate through the collection # => Example: [1, 2, 3]
+#      - Keep tabs on the first digit of the collection # => [1]
+#      - Keep tabs on the rest of the collection # => [2, 3]
+#      - Keep tabs on where you are in the iteration (current index) # => 0
+#      - Iterate through the rest of the collection # => [2, 3]
+#         - Make a range of rest, starting from the current index and extending to the size of the collection minus two
+#            - The first range would be [2]
+#         - Concatenate the first with the range of rest and add it to a combinations collection 
+#         - Keep iterating until the size of the range is less than the size of the collection minus two
+#      - Stop iterating if rest is out of bounds or if the size of rest is less than the desired size (collection minus 1)
+# - Find the max out of the combinations and return it
+
+def delete_digit(num)
+  num_digits = num.to_s.chars
+  combo_size = num_digits.size - 1
+  combinations = []
+
+  num_digits.each_with_index do |first, current_idx|
+    rest = num_digits[current_idx+1..-1]
+    
+    if !rest.nil?
+      rest.size.times do |idx|
+        range = rest[idx, combo_size - 1]
+        if range.size == combo_size - 1
+          combinations << ([first] + range)
+        end
+      end
+    end
+
+  end
+
+  combinations.each_with_index do |combo, idx|
+    combinations[idx] = combo.reduce(:+).to_i
+  end
+
+  combinations.max
+end
+
+p delete_digit(152) == 52
+p delete_digit(1001) == 101
+p delete_digit(10) == 1
+
+# find all pairs
+
+find_pairs(["1", "2", "3"]) == [["1", "2"], ["1, 3"], ["2, 3"]]
+
+# Algo
+# - Iterate through each element of the collection
+#   - Keep tabs on the first of the collection
+#   - Keep tabs on the rest of the collection
+#   - If rest is not out of bounds
+#     - Iterate over the rest of the collection
+#       - Create a sub collection with the first and the current number of rest
+#       - Add the sub collection to a results collection
+#       - Stop iteration when you run out of numbers
+#   - Stop the iteration when you run out of numbers
+# - Return the results collection
+
+def find_pairs(collection)
+  results = []
+  collection.each_with_index do |current_num, idx|
+    first = current_num
+    rest = collection[idx+1..-1]
+
+    if !rest.nil?
+      rest.each do |rest_n|
+        sub_collection = [first, rest_n]
+        results << sub_collection
+      end
+    end
+  
+  end
+  results
+end
+
+find_pairs(["1", "2", "3"]) == [["1", "2"], ["1", "3"], ["2", "3"]]
