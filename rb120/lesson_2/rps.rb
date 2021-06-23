cbf4351431a6c8455321711bbbb1fdcc8de7802c
Ricardo Viejo <raviejo@gmail.com>
2021-06-23 00:16:12 +0000

    Rock Paper Scissors OOP with 2 designs

---
+++ rb120/lesson_2/rps.rb
@@ -0,0 +1,147 @@
+# => Step 1
+# Rock, Paper, Scissors is a two-player game where each player chooses
+# one of three possible moves: rock, paper, or scissors. The chosen moves
+# will then be compared to see who wins, according to the following rules:
+
+# - rock beats scissors
+# - scissors beats paper
+# - paper beats rock
+
+# If the players chose the same move, then it's a tie.
+
+# => Step 2
+# Nouns: player, move, rule
+# Verbs: choose, compare
+
+# => Step 3
+# Player
+# - choose
+# Move
+# Rule
+
+# - compare
+
+# => Step 4
+class Player
+  attr_accessor :move, :name
+  
+  def initialize
+    set_name
+  end
+end
+
+class Human < Player
+  def set_name
+    name = nil
+    loop do
+      puts "What's your name?"
+      name = gets.chomp
+      break unless name.empty?
+    end
+    self.name = name
+  end
+  
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors:"
+      choice = gets.chomp
+      break if ['rock', 'paper', 'scissors'].include?(choice)
+      puts "Sorry, invalid choice."
+    end
+    self.move = choice
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+  end
+  
+  def choose
+    self.move = ['rock', 'paper', 'scissors'].sample
+  end
+end
+
+class Move
+  def initialize
+    # seems like we need something to keep track
+    # of the choice... a move object can be "paper", "rock" or "scissors"
+  end
+end
+
+class Rule
+  def initialize
+    # not sure what the "state" of a rule object should be
+  end
+end
+
+# not sure where "compare" goes yet
+def compare(move1, move2)
+
+end
+
+# Game Orchestration Engine
+# Contains the procedural or imperative code 
+class RPSGame
+  attr_reader :human, :computer
+  
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+  
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp
+      break if ['y','n'].include?(answer.downcase)
+      puts "Sorry, must be y or n."
+    end
+    
+    answer == 'y'
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      human.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+  
+  def display_welcome_message
+    puts "Hi #{human.name}! Welcome to Rock, Paper, Scissors!"
+  end
+  
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors. Good bye!"
+  end
+  
+  def display_winner
+    puts "#{human.name} chose #{human.move}"
+    puts "#{computer.name} chose #{computer.move}"
+    
+    case human.move
+    when 'rock'
+      puts "It's a tie!" if computer.move == 'rock'
+      puts "#{human.name} won!" if computer.move == 'scissors'
+      puts "#{computer.name} won!" if computer.move == 'paper'
+    when 'paper'
+      puts "It's a tie!" if computer.move == 'paper'
+      puts "#{human.name} won!" if computer.move == 'rock'
+      puts "#{computer.name} won!" if computer.move == 'scissors'
+    when 'scissors' 
+      puts "It's a tie!" if computer.move == 'scissors'
+      puts "#{human.name} won!" if computer.move == 'paper'
+      puts "#{computer.name} won!" if computer.move == 'rock'
+    end
+  end
+    
+end
+
+RPSGame.new.play
