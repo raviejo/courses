8847b03de9baac888eda52526174aec2410b4de6
Ricky Viejo <raviejo@gmail.com>
2021-10-27 10:09:00 -0400

    Ruby Challenges: Simple linked list

--- ruby_js_challenges/medium/simple_linked_list.rb
+++ ruby_js_challenges/medium/simple_linked_list.rb
@@ -1,88 +1,72 @@
+require 'pry'
+
 class SimpleLinkedList
   attr_reader :head
-  
+
+  def each
+    elt = @head
+    until elt.nil?
+      yield(elt)
+      elt = elt.next
+    end
+    @head
+  end
+
+  def select
+    result = @head
+    each { |elt| result = @head if yield(elt) }
+    result
+  end
+
   def empty?
     @head.nil?
   end
-  
+
   def push(datum)
-    empty? ? @head = Element.new(datum) : tail.next = Element.new(datum)
+    empty? ? @head = Element.new(datum) : @head = Element.new(datum, @head)
   end
-  
+
   def tail
-    if !empty?
-      current = @head
-      until current.tail? do
-        current = current.next
-      end
-      current
-    else
-      @head
-    end
+    select(&:tail?)
   end
-  
+
   def pop
     current = @head
     if !empty?
-      index = size - 1
-      until index.zero? do
-        current = current.next
-        index -= 1
-      end
-      popped = current.next
-      current.next = nil
-      
-      if current.tail?
-        datum = @head.datum
-        @head = nil
-        datum
-      else
-        popped.datum
-      end
+      popped = @head.datum
+      @head = @head.next
+      popped
     else
       current
     end
   end
-  
+
   def size
     counter = 0
-    if !empty?
-      counter = 1
-      current = @head
-      until current.tail? do
-        current = current.next
-        counter += 1
-      end
-      counter
-    else
-      counter
-    end
+    each { |_| counter += 1 }
+    counter
   end
-  
+
   def peek
     empty? ? nil : @head.datum
   end
-  
+
   def self.from_a(ary)
     list = new
-    ary.each do |datum|
-      list.push(datum)
-    end
+    ary.reverse_each { |datum| list.push(datum) } unless ary.nil?
+    list
   end
-  
+
   def to_a
     ary = []
-    current = @head
-    if !empty?
-      ary << current
-      until current.tail? do
-        ary << current
-        current = current.next
-      end
-      current
-    else
-      current
-    end
+    each { |elt| ary << elt.datum }
+    ary
+  end
+
+  def reverse
+    reversed = SimpleLinkedList.new
+    each { |elt| reversed.push(elt.datum) }
+    reversed
   end
 end
 
@@ -90,19 +74,16 @@ end
 class Element
   attr_reader :datum
   attr_accessor :next
-  
+
   def initialize(datum, next_element = nil)
     @datum = datum
     @next = next_element
   end
-  
+
   def tail?
     @next.nil?
   end
 end
 
-
-list = SimpleLinkedList.new
-list.push(1)
-puts list.pop
+list = SimpleLinkedList.from_a([1, 2])
 puts list.size
--- ruby_js_challenges/medium/simple_linked_list_test.rb
+++ ruby_js_challenges/medium/simple_linked_list_test.rb
@@ -9,52 +9,52 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_element_tail
-    
+
     element = Element.new(1)
     assert element.tail?
   end
 
   def test_element_next_default
-    
+
     element = Element.new(1)
     assert_nil element.next
   end
 
   def test_element_next_initialization
-    
+
     element1 = Element.new(1)
     element2 = Element.new(2, element1)
     assert_equal element1, element2.next
   end
 
   def test_empty_list_size
-    
+
     list = SimpleLinkedList.new
     assert_equal 0, list.size
   end
 
   def test_empty_list_empty
-    
+
     list = SimpleLinkedList.new
     assert list.empty?
   end
 
   def test_pushing_element_on_list
-    
+
     list = SimpleLinkedList.new
     list.push(1)
     assert_equal 1, list.size
   end
 
   def test_empty_list_1_element
-    
+
     list = SimpleLinkedList.new
     list.push(1)
     refute list.empty?
   end
 
   def test_peeking_at_list
-    
+
     list = SimpleLinkedList.new
     list.push(1)
     assert_equal 1, list.size
@@ -62,13 +62,13 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_peeking_at_empty_list
-    
+
     list = SimpleLinkedList.new
     assert_nil list.peek
   end
 
   def test_access_head_element
-    
+
     list = SimpleLinkedList.new
     list.push(1)
     assert_instance_of Element, list.head
@@ -77,7 +77,7 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_items_are_stacked
-    
+
     list = SimpleLinkedList.new
     list.push(1)
     list.push(2)
@@ -87,7 +87,7 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_push_10_items
-    
+
     list = SimpleLinkedList.new
     (1..10).each do |datum|
       list.push(datum)
@@ -105,7 +105,7 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_popping_frenzy
-    
+
     list = SimpleLinkedList.new
     (1..10).each do |datum|
       list.push(datum)
@@ -116,21 +116,21 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_from_a_empty_array
-    
+
     list = SimpleLinkedList.from_a([])
     assert_equal 0, list.size
     assert_nil list.peek
   end
 
   def test_from_a_nil
-    
+
     list = SimpleLinkedList.from_a(nil)
     assert_equal 0, list.size
     assert_nil list.peek
   end
 
   def test_from_a_2_element_array
-    
+
     list = SimpleLinkedList.from_a([1, 2])
     assert_equal 2, list.size
     assert_equal 1, list.peek
@@ -138,7 +138,7 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_from_a_10_items
-    
+
     list = SimpleLinkedList.from_a((1..10).to_a)
     assert_equal 10, list.size
     assert_equal 1, list.peek
@@ -146,13 +146,13 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_to_a_empty_list
-    
+
     list = SimpleLinkedList.new
     assert_equal [], list.to_a
   end
 
   def test_to_a_of_1_element_list
-    
+
     list = SimpleLinkedList.from_a([1])
     assert_equal [1], list.to_a
     assert_equal 1, list.size
@@ -160,7 +160,7 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_to_a_of_2_element_list
-    
+
     list = SimpleLinkedList.from_a([1, 2])
     assert_equal [1, 2], list.to_a
     assert_equal 2, list.size
@@ -169,7 +169,7 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_reverse_2_element_list
-    
+
     list = SimpleLinkedList.from_a([1, 2])
     # list_r and list need not be the same object
     list_r = list.reverse
@@ -180,14 +180,14 @@ class LinkedListTest < Minitest::Test
   end
 
   def test_reverse_10_element_list
-    
+
     data = (1..10).to_a
     list = SimpleLinkedList.from_a(data)
     assert_equal data.reverse, list.reverse.to_a
   end
 
   def test_roundtrip_10_element_array
-    
+
     data = (1..10).to_a
     assert_equal data, SimpleLinkedList.from_a(data).to_a
   end
